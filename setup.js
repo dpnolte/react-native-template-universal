a8ecb68bd7ee786dabc4b66dd8a78b763f14ad3b
Derk <dpnolte@gmail.com>
2019-04-06 17:30:24 +0200

    manual use of setup.js

--- setup.js
+++ setup.js
@@ -24,38 +24,38 @@ templateFilesToDelete.forEach(deleteTemplateFile);
 
 const fileName = "package.json";
 const packagePath = path.resolve(fileName);
-let package = require(packagePath);
+let json = require(packagePath);
 
 console.log(`Adding scripts to package.json`);
-package.scripts["web"] = "react-scripts start";
-package.scripts["build"] = "react-scripts build";
-package.scripts["test:web"] = "react-scripts test --env=jsdom";
-package.scripts["eject"] = "react-scripts eject";
-package.scripts["electron"] =
+json.scripts["web"] = "react-scripts start";
+json.scripts["build"] = "react-scripts build";
+json.scripts["test:web"] = "react-scripts test --env=jsdom";
+json.scripts["eject"] = "react-scripts eject";
+json.scripts["electron"] =
   "yarn concurrently 'yarn browserless-start' 'yarn wait-on http://localhost:3000/; yarn electron-run'";
-package.scripts["electron-run"] =
+json.scripts["electron-run"] =
   "ELECTRON_URL=http://localhost:3000 electron src/index.electron.js";
-package.scripts["elecctron-prod-run"] = "electron src/index.electron.js";
-package.scripts["browserless-start"] = "BROWSER=none react-scripts start";
-package.scripts["lint"] = "tslint './src/**/*.{ts,tsx}'";
-package.scripts["lint:fix"] = "tslint --fix './src/**/*.{ts,tsx}'";
-package.scripts["generate-assets"] = "python ./scripts/generateAssets.py";
-package.scripts["generate-indexes"] = "python ./scripts/generateIndexes.py";
-package.scripts["generate-sourcemap"] =
+json.scripts["elecctron-prod-run"] = "electron src/index.electron.js";
+json.scripts["browserless-start"] = "BROWSER=none react-scripts start";
+json.scripts["lint"] = "tslint './src/**/*.{ts,tsx}'";
+json.scripts["lint:fix"] = "tslint --fix './src/**/*.{ts,tsx}'";
+json.scripts["generate-assets"] = "python ./scripts/generateAssets.py";
+json.scripts["generate-indexes"] = "python ./scripts/generateIndexes.py";
+json.scripts["generate-sourcemap"] =
   "ts-node --files scripts/sourceMapStackTrace.ts generate true";
-package.scripts["generate-unit-tests"] =
+json.scripts["generate-unit-tests"] =
   "ts-node --files scripts/unitTestGenerator.ts";
 
 console.log(`Adding entry point for electron`);
-package["main"] = "index.electron.js";
-/*package["browserList"] = [
+json["main"] = "index.electron.js";
+package["browserList"] = [
   ">0.2%",
   "not dead",
   "not ie <= 11",
   "not op_mini all"
 ];
-delete package["jest"];*/
-fs.writeFileSync(fileName, JSON.stringify(package));
+delete package["jest"];
+fs.writeFileSync(fileName, JSON.stringify(json, null, 2));
 
 
 // TODO: app.json to src/
